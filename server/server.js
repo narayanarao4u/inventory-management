8b2b5ea974cfa71b1094382ed058c84b51d0f622
narayanarao4u <narayanarao4u@yahoo.com>
2025-03-17 17:40:29 +0530

    before creating Repo

--- server/server.js
+++ server/server.js
@@ -1,20 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 import knex from 'knex';
+import dotenv from 'dotenv';
 import { initializeDatabase } from './database.js';
 import routes from './routes.js';
+
+dotenv.config();
 
 const app = express();
 const db = knex({
   client: 'mssql',
   connection: {
-    host: '10.34.130.254',
-    user: 'sa',
-    password: 'bsnl@123',
-    database: 'inventory'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
   }
 });
-const SECRET_KEY = 'your_secret_key';
+const SECRET_KEY = process.env.SECRET_KEY;
 
 app.use(cors());
 app.use(express.json());
@@ -23,7 +26,7 @@ app.use(express.json());
 
 app.use('/api', routes(db, SECRET_KEY));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}/`);
 });
